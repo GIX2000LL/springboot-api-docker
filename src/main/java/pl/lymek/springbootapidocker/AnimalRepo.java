7aad73ae0c38bf088b89083162b6c9a1b01e9d39
GIX2000 <leczyckilukasz@op.pl>
2022-05-08 22:02:09 +0200

    init commit

---
+++ src/main/java/pl/lymek/springbootapidocker/AnimalRepo.java
@@ -0,0 +1,7 @@
+package pl.lymek.springbootapidocker;
+
+//import org.springframework.data.repository.CrudRepository;
+
+public interface AnimalRepo {  // extends    CrudRepository<Animal,Long> {
+
+}
